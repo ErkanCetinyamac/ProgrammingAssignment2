53db3ecc4ed33c4eb4067fdce058e5d49c517c99
Erkan Çetinyamaç <35914359+ErkanCetinyamac@users.noreply.github.com>
2020-11-10 14:28:33 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversions are usually inconvenient processes when it comes to large scale amount of data.
 
-## Write a short comment describing this function
+## Therefore with these functions we cache them rather than recomputed.
+
+## This function creates a special "matrix" object that 
+#can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    invert_Mtx <- NULL
+    set_Mtx <- function (y) {
+        x <<- y
+        invert_Mtx <<- NULL
+    }
+    
+    
+    get_Mtx <- function () {
+        x
+    }
 
-}
+    set_Inverse <- function(inv) {
+        invert_Mtx <<- inv
+    }
+    
+    get_Inverse <- function() {
+        invert_Mtx
+    }
+    
+    list (set_Mtx = set_Mtx, get_Mtx = get_Mtx, set_Inverse = set_Inverse, get_Inverse = get_Inverse)
+  
+    }
 
 
-## Write a short comment describing this function
+
+
+
+
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    
+    invert_Mtx <- x$get_Inverse()
+    
+    if(!is.null(invert_Mtx)) {
+        
+        message("displaying cached inverse..")
+        
+        return(invert_Mtx)
+    }
+    
+    my_data <- x$get_Mtx()
+    invert_Mtx <- solve(my_data, ...)
+    x$set_Inverse(invert_Mtx)
+    invert_Mtx
+    
+       
 }
